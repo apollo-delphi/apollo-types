4771d48aa92d887d45c5733ffadb33d71dad03a8
alezzle <niekras@tut.by>
2021-04-27 11:05:38 +0300

    dev

---
+++ Source/Apollo_Types_Test.dpr
@@ -0,0 +1,18 @@
+program Apollo_Types_Test;
+
+{$STRONGLINKTYPES ON}
+uses
+  Vcl.Forms,
+  System.SysUtils,
+  DUnitX.Loggers.GUI.VCL,
+  DUnitX.Loggers.Xml.NUnit,
+  DUnitX.TestFramework,
+  tstApollo_Types in 'tstApollo_Types.pas',
+  Apollo_Types in 'Apollo_Types.pas';
+
+begin
+  Application.Initialize;
+  Application.Title := 'DUnitX';
+  Application.CreateForm(TGUIVCLTestRunner, GUIVCLTestRunner);
+  Application.Run;
+end.
